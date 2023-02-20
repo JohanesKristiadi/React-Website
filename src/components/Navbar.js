543fd1953232ef072d255e9c2542d8c86c081476
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-02-20 14:21:03 +0700

    membuat link antar halaman

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "../style/Navbar.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const Navbar = () =>{
-    return(
+const Navbar = () => {
+    return (
         <div className="Navbar">
             <h3>Bagian Navbar</h3>
             <ul className="my-ul">
-                <li><Link to ="/"> </Link>Home</li>
-                <li><Link to ="/about"> </Link>About</li>
-                <li><Link to ="/contact"> </Link>contact</li>
+                <li><Link to="/" className="link"> Home</Link></li>
+                <li><Link to="/about" className="link"> About</Link></li>
+                <li><Link to="/contact" className="link"> contact</Link></li>
             </ul>
         </div>
     )
